3a9a32fb4e96e1f5dfc193a4f643ffe238eb4984
remya007 <117816591+remya007@users.noreply.github.com>
2022-11-09 15:45:26 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("enter the number");
+scanf("%d",&number);
+if(number>0)
+{
+printf("the given number is positive");
+}
+else
+{
+printf("the given number is negative");
+}
+}
