3e11ae0be48c0ede0fe0861a55af15fbb0bc4914
remya007 <117816591+remya007@users.noreply.github.com>
2022-11-09 15:03:48 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,average;
+printf("enter three numbers");
+scanf("%d %d %d",&a,&b,&c);
+average=(a+b+c)/3;
+printf("%d",average);
+}
---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("enter a number");
+scanf("%d",&number);
+if(number%2==0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+}
